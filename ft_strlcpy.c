ed50830c1f680a2b6a9040b808c1e28bd629c600
Achraf Ben Kardoud <acben-ka@c4r9p2.1337.ma>
2024-10-24 21:37:47 +0100

    lib

---
+++ ft_strlcpy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+size_t ft_strlen(const char *str)
+{
+    size_t i = 0;
+    while (str[i] != '\0')
+        i++;
+    return i;
+}
+
+size_t ft_strlcpy(char *dest, const char *src, size_t size)
+{
+    size_t len = ft_strlen(src);
+    size_t i = 0;
+
+    if (size > 0)
+    {
+        while (src[i] && i < size - 1)
+        {
+            dest[i] = src[i];
+            i++;
+        }
+    }
+    dest[i] = '\0';
+    return len;
+}
+
+int main()
+{
+    char src[] = "pooooool";
+    char destt[10];
+    printf ("%zu\n", ft_strlcpy(destt, src, 3));
+    printf ("%s", destt);
+}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -18,14 +18,14 @@ char *ft_strrchr(const char *str, int c)
     {
         if (str[len - 1] == (char)c)
             return (char *)&str[len - 1];
-        str--;
+        len--;
     }
     if ((char)c == '\0')
         return (char *)&str[len];
     return NULL;
 }
 int main()
-{       
+{   
     printf("%s", ft_strrchr("achraf", 'a'));
     return 0;
 }

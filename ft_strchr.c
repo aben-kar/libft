ae4a291511cce9aebda509668cbab3fc5a9e612a
your name <you@example.com>
2024-10-24 10:20:17 +0100

    libft

---
+++ ft_strchr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char *ft_strchr(const char *str, int c)
+{
+    while (*str != '\0')
+    {
+        if ((char)c == *str)
+            return (char *)str;
+        str++;
+    }
+    if ((char)c == '\0')
+        return (char *)str;
+    return NULL;
+}
+int main ()
+{
+    printf ("%s", ft_strchr("achraf", 99));
+}

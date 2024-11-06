6494727b96d2524122661f49c8294f6518aad7f7
Achraf Ben Kardoud <acben-ka@c4r2p7.1337.ma>
2024-11-06 16:38:25 +0100

    tfb

--- ft_calloc.c
+++ ft_calloc.c
@@ -15,18 +15,18 @@ void *ft_calloc(size_t num, size_t size) {
     return ptr;
 }
 
-int main() {
-    size_t num = 7;
-    size_t size = sizeof(int);
-    size_t i = 0;
+// int main() {
+//     size_t num = 7;
+//     size_t size = sizeof(int);
+//     size_t i = 0;
     
-    int *array = (int *)ft_calloc(num, size); 
+//     int *array = (int *)ft_calloc(num, size); 
 
-    while (i < num) 
-    {
-        printf("array[%zu] = %d\n\n", i, array[i]);
-        i++;
-    }
-    free(array);
-    return 0;
-}
+//     while (i < num) 
+//     {
+//         printf("array[%zu] = %d\n\n", i, array[i]);
+//         i++;
+//     }
+//     free(array);
+//     return 0;
+// }
--- ft_split.c
+++ ft_split.c
@@ -1,36 +1,44 @@
 #include "libft.h"
 
-int count_word(const char *arr, char sp)
+static int count_word(char  const *s1, char sp)
 {
     int i = 0;
     int count = 0;
     int check = 1;
-
-    while(arr[i])
+    while (s1[i])
     {
-        if (arr[i] != sp && check == 1)
+        if (s1[i] != sp && check == 1)
         {
             count += 1;
             check = 0;
         }
-        else if (arr[i] == sp)
-        {
+        else if(s1[i] == sp)
             check = 1;
-        }
         i++;
     }
     return count;
 }
 
-static int loop(int *i, const char *s, char c)
+static char *ft_strsdup(char const *s, size_t *j, char sp)
 {
-    int count = 0;
-    while (s[(*i)] != c && s[(*i)])
+    char *result;
+    size_t i;
+    size_t k;
+
+    i = *j;
+    while (s[*j] != sp && s[*j])
+        (*j)++;
+    result = (char *)malloc((*j - i + 1) * sizeof(char));
+    if (!result)
+        return (NULL);
+        k = 0;
+    while ( k < *j - i)
     {
-        (*i)++;
-        count++;
+        result[k] = s[i + k];
+        k++;
     }
-    return count;
+    result[k] = '\0';
+    return (result);
 }
 
 static void ft_free(int i, char **prr)
@@ -45,45 +53,39 @@ static void ft_free(int i, char **prr)
 
 char **ft_split(char const *s, char c)
 {
+    size_t j;
+    size_t i;
+
+    i = 0;
+    j = 0;
     if (!s)
         return NULL;
     char **prr;
-    size_t word = count_word(s, c);
-
-    prr = (char **) malloc((word + 1) * sizeof(char *));
-    if (prr == NULL)
-        return NULL;
-    int i = 0;
-    int z = 0;
-    int y = 0;
-    int count = 0;
-    while (i < word)
+    size_t len_word;
+    len_word = count_word(s, c);
+    prr = (char **) malloc((len_word + 1) * sizeof(char *));
+    if (prr == NULL) return NULL;
+    while (j < len_word)
     {
-        while (s[z] == c && s[z])
-            z++;
-        count = loop(&z, s, c);
-        prr[i] = malloc(sizeof(char) * count + 1);
-        if (!prr[i])
+        while (s[i] == c && s[i])
+            i++;
+        prr[j] = ft_strsdup(s, &i, c);
+        if (!prr[j])
         {
-            ft_free(i, prr);
+            ft_free(j - 1, prr);
             return (NULL);
         }
-        while (s[y] == c && s[y])
-            y++;
-        ft_strlcpy(prr[i], s + y, count + 1);
-        y = z;
-        i++;
+        j++;
     }
-    prr[i] = NULL;
-    return (prr);
+    return prr;
 }
 
 int main()
 {
-    char **arr = NULL;
-
-    arr = ft_split("          ", ' ');
-    for(int i = 0; i < 2; i++)
+    char **arr;
+    
+    arr = ft_split(" av ab an ", ' ');
+    for(int i = 0; i < 3; i++)
     {
         printf("%s\n", arr[i]);
     }

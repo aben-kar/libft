b64b3ebc8a5c1e19cf33803ec18f725d41176062
Achraf Ben Kardoud <acben-ka@c4r3p3.1337.ma>
2024-11-09 16:16:00 +0100

    ld

--- ft_striteri.c
+++ ft_striteri.c
@@ -2,26 +2,23 @@
 
 void my_upper(unsigned int i, char *str)
 {
-    if (*str >= 'a' && *str <= 'z')
+    if(*str >= 'a' && *str <= 'z')
         *str -= 32;
-    //return str;
 }
 void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    int i;
+    int i = 0;
 
-    i = 0;
     while (s[i])
     {
         f(i, &s[i]);
         i++;
     }
 }
-
 int main()
 {
-    char str[] = "hello";
-    ft_striteri(str, my_upper);
-    printf ("%s", str);
-
+    char s[] = "hello world";
+    ft_striteri(s, my_upper);
+    printf("%s", s);
+    return 0;
 }
